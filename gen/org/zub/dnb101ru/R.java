a6be7c77897cdd4985f16c032f73e3af8a2356e8
Skalozubov <skalozubov@users.noreply.github.com>
2013-02-10 22:22:34 +0000

    Added download notification.

--- gen/org/zub/dnb101ru/R.java
+++ gen/org/zub/dnb101ru/R.java
@@ -11,19 +11,22 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo=0x7f020001;
-        public static final int logo_=0x7f020002;
+        public static final int av_download=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
+        public static final int logo_=0x7f020003;
     }
     public static final class id {
-        public static final int Volumelevel=0x7f070003;
-        public static final int VolumelevelHIGH=0x7f070005;
-        public static final int VolumelevelLOW=0x7f070004;
-        public static final int button1=0x7f070000;
-        public static final int imageView1=0x7f070006;
-        public static final int item1=0x7f070008;
-        public static final int menu_settings=0x7f070007;
-        public static final int seekBar=0x7f070001;
+        public static final int Volumelevel=0x7f070004;
+        public static final int VolumelevelHIGH=0x7f070006;
+        public static final int VolumelevelLOW=0x7f070005;
+        public static final int button1=0x7f070001;
+        public static final int button2=0x7f070003;
+        public static final int imageView1=0x7f070007;
+        public static final int item1=0x7f070009;
+        public static final int item2=0x7f070008;
+        public static final int item3=0x7f07000a;
+        public static final int seekBar=0x7f070000;
         public static final int songTitle=0x7f070002;
     }
     public static final class layout {
